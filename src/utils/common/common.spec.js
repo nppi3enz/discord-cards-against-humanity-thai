a192ccc57a16d613ed9a75bd6a536a306a06d5ed
moonstar-x <christian.lopez99@outlook.com>
2019-07-20 00:22:03 -0500

    Fixed message split common method. (#5)

--- src/utils/common/common.spec.js
+++ src/utils/common/common.spec.js
@@ -20,9 +20,11 @@ describe('Utils: Common Methods', () => {
   });
 
   test('should return an array where the elements are less in size than the limit allowed.', () => {
-    const stringArray = 'string,'.repeat(3).split(',');
+    let stringArray = 'string,'.repeat(3).split(',');
     const longString = 'abc '.repeat(250);
+    const repeatedMessages = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'.repeat(300).split('.');
     stringArray.unshift(longString);
+    stringArray = stringArray.concat(repeatedMessages);
     const actualResult = splitForMessageEmbedField(stringArray);
     actualResult.forEach((item) => {
       expect(item.length).toBeLessThan(MAX_EMBED_FIELD_SIZE);
@@ -30,11 +32,13 @@ describe('Utils: Common Methods', () => {
   });
 
   test('should return an array where its joined elements are equivalent to the joined elements of the argument.', () => {
-    const stringArray = 'string,'.repeat(3).split(',');
+    let stringArray = 'string,'.repeat(3).split(',');
     const longString = 'abc '.repeat(250);
     const messageWithNewlines = 'this is a message with \n newlines to test \n that newlines are also \n added.';
+    const repeatedMessages = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'.repeat(300).split('.');
     stringArray.unshift(longString);
     stringArray.push(messageWithNewlines);
+    stringArray = stringArray.concat(repeatedMessages);
     const actualResult = splitForMessageEmbedField(stringArray).join('');
     const expectedResult = stringArray.join('');
     expect(actualResult).toBe(expectedResult);
