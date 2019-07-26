ee0e9c7083d92a68a56cb1fc7aec51863fffc741
tanb01 <42753752+tanb01@users.noreply.github.com>
2019-07-26 09:29:25 +0200

    Added Player class. (#9)

---
+++ src/classes/Player.js
@@ -0,0 +1,43 @@
+class Player {
+  constructor(guildMember, isGamemaster = false) {
+    this._name = guildMember.displayName;
+    this._id = guildMember.id;
+    this._member = guildMember;
+    this._isGamemaster = isGamemaster;
+    this._score = 0;
+  }
+
+  incrementScore() {
+    const newScore = this._score++;
+    this._score = newScore;
+    return newScore;
+  }
+
+  set isGamemaster(newValue) {
+    if (typeof newValue === 'boolean') {
+      this._isGamemaster = newValue;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get member() {
+    return this._member;
+  }
+
+  get isGamemaster() {
+    return this._isGamemaster;
+  }
+
+  get score() {
+    return this._score;
+  }
+}
+
+module.exports = Player;
